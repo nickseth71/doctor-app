ff8835ec1c64a25d218c907b17ce3685574de1ff
nickseth71 <nickseth71@gmail.com>
2022-12-06 16:56:24 +0530

    bhupendra 6 dec

--- src/swDev.js
+++ src/swDev.js
@@ -1,7 +1,7 @@
 export default function swDev() {
     let swUrl = `${process.env.PUBLIC_URL}/Sw.js`;
     navigator.serviceWorker.register(swUrl).then((response) => {
-        // console.log('response', response);
+        console.log('response', response);
     })
 }
 
