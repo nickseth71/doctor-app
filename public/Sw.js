b4792a5b5e63ac39ec621596be66e8e16ec6424a
nickseth71 <nickseth71@gmail.com>
2022-12-07 12:00:36 +0530

    bhupendra 7 dec

--- public/Sw.js
+++ public/Sw.js
@@ -49,7 +49,7 @@ const cache_url = [
     "/Page_104"
 ]
 this.addEventListener('install', (event) => {
-
+    // CI = false npm run build
     event.waitUntil(
         caches.open(cacheData).then((cache) => {
             cache.addAll(cache_url);
@@ -58,6 +58,7 @@ this.addEventListener('install', (event) => {
 });
 
 this.addEventListener("fetch", (event) => {
+
     if (!navigator.onLine) {
         event.respondWith(
             caches.match(event.request).then((result) => {
